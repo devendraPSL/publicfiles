0840d3f99ce2f0269c03392428331aae9f1d2c48
root <root@test0lakehdpnode.southeastasia.cloudapp.azure.com>
2016-11-29 08:28:11 +0000

    new changes

--- usersAdd.sh
+++ usersAdd.sh
@@ -4,8 +4,8 @@ echo "$1        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 # Partitions all data disks attached to the VM and creates
 # a RAID-0 volume with them.
 #  https://raw.githubusercontent.com/Azure/azure-quickstart-templates/875d139c16c9c023dce519e6dd48c707e3473346/slurm-on-sles12-hpc/azuredeploy.sh
-setup_data_disks()
-{
+#setup_data_disks()
+#{
     mountPoint="$2"
 	createdPartitions=""
 
@@ -25,4 +25,4 @@ EOF
         createdPartitions="$createdPartitions /dev/${disk}1"
 	done
 
-}
+#}
