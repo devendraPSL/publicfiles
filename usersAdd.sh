341759869b53fca805d88a561aebe5b78b55dbb6
devendraPSL <devendra_asane@persistent.co.in>
2016-11-29 10:26:17 +0000

    some more

--- usersAdd.sh
+++ usersAdd.sh
@@ -4,15 +4,10 @@ echo "$1        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 # Partitions all data disks attached to the VM and creates
 # a RAID-0 volume with them.
 #  https://raw.githubusercontent.com/Azure/azure-quickstart-templates/875d139c16c9c023dce519e6dd48c707e3473346/slurm-on-sles12-hpc/azuredeploy.sh
-#setup_data_disks()
-#{
-    mountPoint="$2"
-	createdPartitions=""
 
     # Loop through and partition disks until not found
-    for disk in sda; do
-        fdisk -l /dev/$disk || break
-        fdisk /dev/$disk << EOF
+
+        fdisk /dev/sda << EOF
 d
 n
 p
@@ -22,7 +17,17 @@ p
 a
 w
 EOF
-        createdPartitions="$createdPartitions /dev/${disk}1"
-	done
 
-#}
+
+        fdisk /dev/sdc << EOF
+n
+p
+1
+
+
+t
+fd
+w
+EOF
+
+
