bb23797678c683f32eff0a5aaa716f2071c0adda
Devendra Asane <devendra_asane@persistent.com>
2016-12-27 15:38:48 +0530

    Update usersAdd.sh

--- usersAdd.sh
+++ usersAdd.sh
@@ -2,16 +2,17 @@ echo "$1        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
 (echo d; echo n; echo p; echo 1; echo; echo; echo a; echo w) | fdisk /dev/sda > /dev/null 
 
-(echo n; echo p; echo 1; echo; echo; echo w) | fdisk /dev/sdc > /dev/null 
-mkfs -t ext4 /dev/sdc1 > /dev/null 
-sleep 5
+if  fdisk -l | grep -q "/dev/sdc" 
+  then 
 
-if [ -e "/dev/sdc1" ]  
-then
+  (echo n; echo p; echo 1; echo; echo; echo w) | fdisk /dev/sdc > /dev/null 
+  mkfs -t ext4 /dev/sdc1 > /dev/null 
+  sleep 5
   mkdir /datadisk0 
   echo "/dev/sdc1    /datadisk0/    ext4    defaults 0 0" >> /etc/fstab
   sleep 2
   mount /dev/sdc1 /datadisk0
+  chmod go+w /datadisk0
 fi
-chmod go+w /datadisk0
+
 
