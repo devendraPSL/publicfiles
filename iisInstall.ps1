2e95ccfae356ba884fafe7877ebd5806a5376538
Devendra Asane <devendra_asane@persistent.com>
2018-08-30 18:59:15 +0530

    Add files via upload

---
+++ iisInstall.ps1
@@ -0,0 +1,80 @@
+Configuration InstallIIS
+{
+
+Param ( [string] $nodeName, [string] $vmNumber, [string] $backendCert, [string] $backendCertPw, [string] $backendCertDnsName, [string] $webConfigPath, [string] $defaultHtmPath )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+Node $nodeName
+  {
+    WindowsFeature WebServerRole
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementConsole
+    {
+      Name = "Web-Mgmt-Console"
+      Ensure = "Present"
+    }
+    WindowsFeature WebManagementService
+    {
+      Name = "Web-Mgmt-Service"
+      Ensure = "Present"
+    }
+    WindowsFeature ASPNet45
+    {
+      Name = "Web-Asp-Net45"
+      Ensure = "Present"
+    }
+    WindowsFeature HTTPRedirection
+    {
+      Name = "Web-Http-Redirect"
+      Ensure = "Present"
+    }
+    WindowsFeature CustomLogging
+    {
+      Name = "Web-Custom-Logging"
+      Ensure = "Present"
+    }
+    WindowsFeature LogginTools
+    {
+      Name = "Web-Log-Libraries"
+      Ensure = "Present"
+    }
+    WindowsFeature RequestMonitor
+    {
+      Name = "Web-Request-Monitor"
+      Ensure = "Present"
+    }
+    WindowsFeature Tracing
+    {
+      Name = "Web-Http-Tracing"
+      Ensure = "Present"
+    }
+    WindowsFeature BasicAuthentication
+    {
+      Name = "Web-Basic-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature WindowsAuthentication
+    {
+      Name = "Web-Windows-Auth"
+      Ensure = "Present"
+    }
+    WindowsFeature ApplicationInitialization
+    {
+      Name = "Web-AppInit"
+      Ensure = "Present"
+    }
+    Package UrlRewrite
+    {
+        Ensure = "Present"
+        Name = "IIS URL Rewrite Module 2"
+        Path = "https://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi"
+        ProductId = "08F0318A-D113-4CF0-993E-50F191D397AD"
+        DependsOn = "[WindowsFeature]WebServerRole"
+    }
+
+  }
+}
